4dd09314837756eadb5b7e2f26d25f9d5c0c8aa4
BuildTools <unconfigured@null.spigotmc.org>
2021-11-21 21:44:52 +0100

    Move to Laptop

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,55 +1,34 @@
-let count = 1;
-let current = 0;
-let isEditing = [
-    [0, false],
-];
-
 $(document).ready(function() {
-
     $("#add").click(function() {
-        if(count === 14) {
-            return;
+        let count = 0;
+        
+        for(let i in $(".element").length) {
+            count++;
         }
-        $("#e" + (count - 1)).after("" +
-            "<div id=\"e" + (count + 1) + "\" class=\"element\">\n" +
-            "        <label>Question :\n" +
-            "            <input value=\"ergettrhbsqemlkj\" type=\"text\" disabled/>\n" +
-            "        </label>\n" +
-            "        <p class=\"answer\">\n" +
-            "            ergerihol\n" +
-            "        </p>\n" +
-            "        <div class=\"edit\"></div>\n" +
-            "        <div class=\"delete\"></div>\n" +
-            "    </div>");
-        count++;
-        isEditing.push([count, false]);
-
-        $(".delete").click(function() {
-            $("#e0").remove();
-        });
+        $("#add").before("" +
+            "<div class='element' id='e'" +
+            "   <label>Question :" +
+            "       <input disabled type='text' value='Question random'/>" +
+            "   </label>" +
+            "   <p class='answer'>Réponse</p>" +
+            "   <div class='edit'></div>" +
+            "   <div class='delete'></div>" +
+            "</div>"
+        );
+        $("#e").attr("e", "e" + count);
     }).hover(function() {
         $(this).css("background-color", "#00FA9AFF");
         $(this).css("transition-duration", ".5s");
     }).mouseout(function() {
-        $(this).css("background-color", "#FF8C00FF");
+        $(this).css("background-color", "#FFFF00");
         $(this).css("transition-duration", ".5s");
     });
 
     $(".edit").click(function() {
-        current = $(".edit").attr("id");
-        console.log(isEditing)
-        console.log(current);
-        let id = $("#e" + current + " input");
-        isEditing[current][1] = !isEditing[current][1];
-
-        if(isEditing[current][1]) {
-            id.prop("disabled", isEditing[current][1]).blur();
-        } else {
-            id.prop("disabled", isEditing[current][1]).focus();
-        }
+        $(this).parent("div");
     });
 
     $(".delete").click(function() {
-        $("#e0").remove();
+        $(this).parent("div").remove();
     });
 });
