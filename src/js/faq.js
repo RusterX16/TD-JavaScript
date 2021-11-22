595eaf772d7b8a509373fecc8fda462cee8e7b3f
Eliott <EliottRuster@gmail.com>
2021-11-22 21:28:32 +0100

    Move to PC

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,34 +1,48 @@
 $(document).ready(function() {
     $("#add").click(function() {
-        let count = 0;
-        
-        for(let i in $(".element").length) {
-            count++;
-        }
-        $("#add").before("" +
-            "<div class='element' id='e'" +
-            "   <label>Question :" +
+        $("#bottombar").before("" +
+            "<div class='element'" +
+            "   <label>Question : " +
             "       <input disabled type='text' value='Question random'/>" +
             "   </label>" +
-            "   <p class='answer'>Réponse</p>" +
-            "   <div class='edit'></div>" +
-            "   <div class='delete'></div>" +
+            "   <p class='answer'></p>" +
+            "   <div class='tools'>" +
+            "       <div class='edit' onclick='editElement(this)'></div>" +
+            "       <div class='delete' onclick='deleteElement(this)'></div>" +
+            "   </div>" +
             "</div>"
         );
-        $("#e").attr("e", "e" + count);
+        $("#count").html($(".element").length);
     }).hover(function() {
-        $(this).css("background-color", "#00FA9AFF");
+        $(this).css("background-color", "#7CFC00");
         $(this).css("transition-duration", ".5s");
     }).mouseout(function() {
         $(this).css("background-color", "#FFFF00");
         $(this).css("transition-duration", ".5s");
-    });
-
-    $(".edit").click(function() {
-        $(this).parent("div");
-    });
-
-    $(".delete").click(function() {
-        $(this).parent("div").remove();
+    }).click(function() {
+        $(this).css("background-color", "#FF8C00");
+        $(this).css("transition-duration", ".5s");
     });
 });
+
+function editElement(field) {
+    let parent = $(field).parent("div").parent("div");
+    let input = parent.children("input");
+    let output = parent.children(".answer");
+
+    if(input.attr("disabled")) {
+        input.attr("disabled", false);
+        input.focus();
+    } else {
+        input.attr("disabled", true);
+        input.blur();
+
+        let answer = prompt("Quelle est la réponse à \n" + input.value);
+        output.html(answer);
+    }
+}
+
+function deleteElement(field) {
+    $(field).parent("div").parent("div").remove();
+    $("#count").html($(".element").length);
+}
